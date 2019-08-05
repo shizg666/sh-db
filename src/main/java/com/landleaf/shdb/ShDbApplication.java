1f95a1c4493ae5bbff9a498c88fbaec846a436c5
lujingyao <ljy88808@gmail.com>
2019-08-05 16:29:16 +0800

    shdb

---
+++ src/main/java/com/landleaf/shdb/ShDbApplication.java
@@ -0,0 +1,13 @@
+package com.landleaf.shdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShDbApplication.class, args);
+    }
+
+}
