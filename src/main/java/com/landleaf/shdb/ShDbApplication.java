4fa452665369a4cbe29be00f7e7a3b77635c886b
shizg <shizengguang@landleaf-tech.com>
2019-08-06 16:04:53 +0800

    swagger

--- src/main/java/com/landleaf/shdb/ShDbApplication.java
+++ src/main/java/com/landleaf/shdb/ShDbApplication.java
@@ -1,13 +1,49 @@
 package com.landleaf.shdb;
 
+import io.swagger.annotations.Api;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
+@MapperScan("com.landleaf.shdb.*")
+@ComponentScan(basePackages = "com.landleaf.shdb.*")
 public class ShDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShDbApplication.class, args);
+    }
+
+    /**
+     * 配置swagger.
+     */
+    @Bean
+    public Docket createRestApi() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("sh")
+                .description("sh管理系统")
+                .version("1.0")
+                .contact(new Contact("wenyilu", "", "wenyilu@landleaf-tech.com"))
+                .build();
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("sh")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.landleaf.shdb"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
